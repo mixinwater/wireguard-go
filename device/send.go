aae447e4285ca56b1790eb36d4e5a9090fbca3bf
Your Name <you@example.com>
2025-12-30 01:59:21 +0330

    minimize performance impact

--- device/send.go
+++ device/send.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/binary"
 	"errors"
+	"math/big"
 	"net"
 	"os"
 	"sync"
@@ -26,16 +27,12 @@ const (
 	profileJumbo  sizeProfile = 1200
 )
 
-var sizeProfiles = []sizeProfile{
-	profileSmall,
-	profileMedium,
-	profileLarge,
-	profileJumbo,
-}
+var sizeProfiles = [4]sizeProfile{profileSmall, profileMedium, profileLarge, profileJumbo}
 
 type obfuscationState struct {
 	sizeProfile     sizeProfile
-	lastRealTraffic atomic.Int64
+	lastRealTraffic int64
+	coinCounter     uint32
 }
 
 type QueueOutboundElement struct {
@@ -52,8 +49,8 @@ type QueueOutboundElementsContainer struct {
 }
 
 const (
-	handshakePrePacketMin = 16
-	handshakePrePacketMax = 32
+	coverSizeMin = 16
+	coverSizeMax = 32
 )
 
 var (
@@ -79,8 +76,8 @@ func initPeerObfuscation(peer *Peer) *obfuscationState {
 	state := &obfuscationState{}
 	var profileIndex [1]byte
 	rand.Read(profileIndex[:])
-	state.sizeProfile = sizeProfiles[int(profileIndex[0])%len(sizeProfiles)]
-	state.lastRealTraffic.Store(time.Now().Unix())
+	state.sizeProfile = sizeProfiles[int(profileIndex[0])&3]
+	state.lastRealTraffic = time.Now().Unix()
 	peerObfuscation[peer] = state
 	return state
 }
@@ -96,70 +93,64 @@ func shouldSendCamouflage(peer *Peer) bool {
 	if state == nil {
 		return false
 	}
-	lastTraffic := state.lastRealTraffic.Load()
-	if time.Now().Unix()-lastTraffic > 60 {
+	now := time.Now().Unix()
+	lastTraffic := atomic.LoadInt64(&state.lastRealTraffic)
+	if now-lastTraffic > 60 {
 		return false
 	}
-	var coin [1]byte
-	rand.Read(coin[:])
-	return coin[0]%100 == 0
+	counter := atomic.AddUint32(&state.coinCounter, 1)
+	return (counter & 0xFF) == 0
 }
 
-func sendCamouflagePacket(peer *Peer) error {
+func sendCamouflagePacket(peer *Peer) {
 	state := getPeerObfuscation(peer)
 	if state == nil {
-		return nil
+		return
 	}
 	targetSize := int(state.sizeProfile)
 	b := make([]byte, targetSize)
 	rand.Read(b)
-	return peer.SendBuffers([][]byte{b})
+	peer.SendBuffers([][]byte{b})
+}
+
+func crandInt(min, max int) (int, error) {
+	if max <= min {
+		return min, nil
+	}
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
+	if err != nil {
+		return 0, err
+	}
+	return min + int(n.Int64()), nil
 }
 
 func sendHandshakePrePackets(peer *Peer) {
-	var sizeBytes [1]byte
-	rand.Read(sizeBytes[:])
-	size := handshakePrePacketMin + int(sizeBytes[0])%(handshakePrePacketMax-handshakePrePacketMin+1)
-
-	prePacket := make([]byte, size)
-	rand.Read(prePacket)
-
-	peer.SendBuffers([][]byte{prePacket})
-
-	var doSecond [1]byte
-	rand.Read(doSecond[:])
-	if doSecond[0]%2 == 0 {
-		time.Sleep(5 * time.Millisecond)
-		rand.Read(sizeBytes[:])
-		size = handshakePrePacketMin + int(sizeBytes[0])%(handshakePrePacketMax-handshakePrePacketMin+1)
-		prePacket = make([]byte, size)
-		rand.Read(prePacket)
-		peer.SendBuffers([][]byte{prePacket})
+	sz, _ := crandInt(coverSizeMin, coverSizeMax)
+	if sz > 0 {
+		b := make([]byte, sz)
+		rand.Read(b)
+		peer.SendBuffers([][]byte{b})
 	}
 }
 
-func padToProfile(pkt []byte, profile sizeProfile, maxSize int) []byte {
+func padToProfileFast(pkt []byte, profile sizeProfile, maxSize int) []byte {
 	targetSize := int(profile)
 	if targetSize > maxSize {
 		targetSize = maxSize
 	}
-
 	currentSize := len(pkt)
 	if currentSize >= targetSize {
 		return pkt
 	}
-
 	paddingSize := targetSize - currentSize
-	if cap(pkt) >= currentSize+paddingSize {
-		pkt = pkt[:currentSize+paddingSize]
-		for i := currentSize; i < len(pkt); i++ {
+	if cap(pkt) >= targetSize {
+		pkt = pkt[:targetSize]
+		for i := currentSize; i < targetSize; i++ {
 			pkt[i] = 0
 		}
 		return pkt
 	}
-
-	padding := make([]byte, paddingSize)
-	return append(pkt, padding...)
+	return append(pkt, make([]byte, paddingSize)...)
 }
 
 func onesComplementSum(data []byte) uint32 {
@@ -185,47 +176,36 @@ func clampTCPMSSv4(pkt []byte, tunMTU int) bool {
 	if len(pkt) < ipv4.HeaderLen {
 		return false
 	}
-
 	ihl := int((pkt[0] & 0x0F) << 2)
 	if ihl < ipv4.HeaderLen || len(pkt) < ihl+20 {
 		return false
 	}
-
 	if pkt[9] != 6 {
 		return false
 	}
-
 	totalLen := int(binary.BigEndian.Uint16(pkt[2:4]))
 	if totalLen == 0 || totalLen > len(pkt) {
 		totalLen = len(pkt)
 	}
-
 	ipHdr := pkt[:ihl]
 	tcp := pkt[ihl:totalLen]
-
 	if len(tcp) < 20 {
 		return false
 	}
-
 	dataOffset := int((tcp[12] >> 4) * 4)
 	if dataOffset < 20 || len(tcp) < dataOffset {
 		return false
 	}
-
-	flags := tcp[13]
-	if (flags & 0x02) == 0 {
+	if (tcp[13] & 0x02) == 0 {
 		return false
 	}
-
 	targetMSS := tunMTU - 40
 	if targetMSS < 536 {
 		targetMSS = 536
 	}
-
 	opts := tcp[20:dataOffset]
 	changed := false
 	i := 0
-
 	for i < len(opts) {
 		kind := opts[i]
 		if kind == 0 {
@@ -252,18 +232,14 @@ func clampTCPMSSv4(pkt []byte, tunMTU int) bool {
 		}
 		i += optLen
 	}
-
 	if !changed {
 		return false
 	}
-
 	tcp[16] = 0
 	tcp[17] = 0
-
 	srcIP := ipHdr[12:16]
 	dstIP := ipHdr[16:20]
 	tcpLen := uint16(len(tcp))
-
 	var sum uint32
 	pseudoHeader := make([]byte, 12)
 	copy(pseudoHeader[0:4], srcIP)
@@ -271,13 +247,10 @@ func clampTCPMSSv4(pkt []byte, tunMTU int) bool {
 	pseudoHeader[8] = 0
 	pseudoHeader[9] = 6
 	binary.BigEndian.PutUint16(pseudoHeader[10:12], tcpLen)
-
 	sum += onesComplementSum(pseudoHeader)
 	sum += onesComplementSum(tcp)
-
 	checksum := finalizeChecksum(sum)
 	binary.BigEndian.PutUint16(tcp[16:18], checksum)
-
 	return true
 }
 
@@ -285,30 +258,24 @@ func clampTCPMSSv6(pkt []byte, tunMTU int) bool {
 	if len(pkt) < 60 || pkt[6] != 6 {
 		return false
 	}
-
 	tcp := pkt[40:]
 	if len(tcp) < 20 {
 		return false
 	}
-
 	dataOffset := int((tcp[12] >> 4) * 4)
 	if dataOffset < 20 || len(tcp) < dataOffset {
 		return false
 	}
-
 	if tcp[13]&0x02 == 0 {
 		return false
 	}
-
 	targetMSS := tunMTU - 60
 	if targetMSS < 1220 {
 		targetMSS = 1220
 	}
-
 	opts := tcp[20:dataOffset]
 	changed := false
 	i := 0
-
 	for i < len(opts) {
 		kind := opts[i]
 		if kind == 0 {
@@ -335,32 +302,26 @@ func clampTCPMSSv6(pkt []byte, tunMTU int) bool {
 		}
 		i += optLen
 	}
-
 	if !changed {
 		return false
 	}
-
 	tcp[16] = 0
 	tcp[17] = 0
-
 	var sum uint32
 	for i := 8; i < 40; i += 2 {
 		sum += uint32(pkt[i])<<8 | uint32(pkt[i+1])
 	}
 	sum += uint32(len(tcp))
 	sum += 6
-
 	for i := 0; i+1 < len(tcp); i += 2 {
 		sum += uint32(tcp[i])<<8 | uint32(tcp[i+1])
 	}
 	if len(tcp)&1 != 0 {
 		sum += uint32(tcp[len(tcp)-1]) << 8
 	}
-
 	for sum > 0xFFFF {
 		sum = (sum & 0xFFFF) + (sum >> 16)
 	}
-
 	binary.BigEndian.PutUint16(tcp[16:], ^uint16(sum))
 	return true
 }
@@ -400,14 +361,12 @@ func (peer *Peer) SendHandshakeInitiation(isRetry bool) error {
 	if !isRetry {
 		peer.timers.handshakeAttempts.Store(0)
 	}
-
 	peer.handshake.mutex.RLock()
 	if time.Since(peer.handshake.lastSentHandshake) < RekeyTimeout {
 		peer.handshake.mutex.RUnlock()
 		return nil
 	}
 	peer.handshake.mutex.RUnlock()
-
 	peer.handshake.mutex.Lock()
 	if time.Since(peer.handshake.lastSentHandshake) < RekeyTimeout {
 		peer.handshake.mutex.Unlock()
@@ -415,30 +374,23 @@ func (peer *Peer) SendHandshakeInitiation(isRetry bool) error {
 	}
 	peer.handshake.lastSentHandshake = time.Now()
 	peer.handshake.mutex.Unlock()
-
 	peer.device.log.Verbosef("%v - Sending handshake initiation", peer)
-
-	sendHandshakePrePackets(peer)
-
+	go sendHandshakePrePackets(peer)
 	msg, err := peer.device.CreateMessageInitiation(peer)
 	if err != nil {
 		peer.device.log.Errorf("%v - Failed to create initiation message: %v", peer, err)
 		return err
 	}
-
 	packet := make([]byte, MessageInitiationSize)
 	_ = msg.marshal(packet)
 	peer.cookieGenerator.AddMacs(packet)
-
 	peer.timersAnyAuthenticatedPacketTraversal()
 	peer.timersAnyAuthenticatedPacketSent()
-
 	err = peer.SendBuffers([][]byte{packet})
 	if err != nil {
 		peer.device.log.Errorf("%v - Failed to send handshake initiation: %v", peer, err)
 	}
 	peer.timersHandshakeInitiated()
-
 	return err
 }
 
@@ -446,29 +398,23 @@ func (peer *Peer) SendHandshakeResponse() error {
 	peer.handshake.mutex.Lock()
 	peer.handshake.lastSentHandshake = time.Now()
 	peer.handshake.mutex.Unlock()
-
 	peer.device.log.Verbosef("%v - Sending handshake response", peer)
-
 	response, err := peer.device.CreateMessageResponse(peer)
 	if err != nil {
 		peer.device.log.Errorf("%v - Failed to create response message: %v", peer, err)
 		return err
 	}
-
 	packet := make([]byte, MessageResponseSize)
 	_ = response.marshal(packet)
 	peer.cookieGenerator.AddMacs(packet)
-
 	err = peer.BeginSymmetricSession()
 	if err != nil {
 		peer.device.log.Errorf("%v - Failed to derive keypair: %v", peer, err)
 		return err
 	}
-
 	peer.timersSessionDerived()
 	peer.timersAnyAuthenticatedPacketTraversal()
 	peer.timersAnyAuthenticatedPacketSent()
-
 	err = peer.SendBuffers([][]byte{packet})
 	if err != nil {
 		peer.device.log.Errorf("%v - Failed to send handshake response: %v", peer, err)
@@ -478,18 +424,15 @@ func (peer *Peer) SendHandshakeResponse() error {
 
 func (device *Device) SendHandshakeCookie(initiatingElem *QueueHandshakeElement) error {
 	device.log.Verbosef("Sending cookie response for denied handshake message for %v", initiatingElem.endpoint.DstToString())
-
 	sender := binary.LittleEndian.Uint32(initiatingElem.packet[4:8])
 	reply, err := device.cookieChecker.CreateReply(initiatingElem.packet, sender, initiatingElem.endpoint.DstToBytes())
 	if err != nil {
 		device.log.Errorf("Failed to create cookie reply: %v", err)
 		return err
 	}
-
 	packet := make([]byte, MessageCookieReplySize)
 	_ = reply.marshal(packet)
 	device.net.bind.Send([][]byte{packet}, initiatingElem.endpoint)
-
 	return nil
 }
 
@@ -510,9 +453,7 @@ func (device *Device) RoutineReadFromTUN() {
 		device.state.stopping.Done()
 		device.queue.encryption.wg.Done()
 	}()
-
 	device.log.Verbosef("Routine: TUN reader - started")
-
 	var (
 		batchSize   = device.BatchSize()
 		readErr     error
@@ -523,12 +464,10 @@ func (device *Device) RoutineReadFromTUN() {
 		sizes       = make([]int, batchSize)
 		offset      = MessageTransportHeaderSize
 	)
-
 	for i := range elems {
 		elems[i] = device.NewOutboundElement()
 		bufs[i] = elems[i].buffer[:]
 	}
-
 	defer func() {
 		for _, elem := range elems {
 			if elem != nil {
@@ -537,19 +476,15 @@ func (device *Device) RoutineReadFromTUN() {
 			}
 		}
 	}()
-
 	tunMTU := int(device.tun.mtu.Load())
-
 	for {
 		count, readErr = device.tun.device.Read(bufs, sizes, offset)
 		for i := 0; i < count; i++ {
 			if sizes[i] < 1 {
 				continue
 			}
-
 			elem := elems[i]
 			elem.packet = bufs[i][offset : offset+sizes[i]]
-
 			if len(elem.packet) >= 40 {
 				ipVersion := elem.packet[0] >> 4
 				if ipVersion == 4 {
@@ -558,7 +493,6 @@ func (device *Device) RoutineReadFromTUN() {
 					clampTCPMSSv6(elem.packet, tunMTU)
 				}
 			}
-
 			var peer *Peer
 			switch elem.packet[0] >> 4 {
 			case 4:
@@ -567,25 +501,20 @@ func (device *Device) RoutineReadFromTUN() {
 				}
 				dst := elem.packet[IPv4offsetDst : IPv4offsetDst+net.IPv4len]
 				peer = device.allowedips.Lookup(dst)
-
 			case 6:
 				if len(elem.packet) < ipv6.HeaderLen {
 					continue
 				}
 				dst := elem.packet[IPv6offsetDst : IPv6offsetDst+net.IPv6len]
 				peer = device.allowedips.Lookup(dst)
-
 			default:
 				device.log.Verbosef("Received packet with unknown IP version")
 			}
-
 			if peer == nil {
 				continue
 			}
-
 			state := initPeerObfuscation(peer)
-			state.lastRealTraffic.Store(time.Now().Unix())
-
+			atomic.StoreInt64(&state.lastRealTraffic, time.Now().Unix())
 			elemsForPeer, ok := elemsByPeer[peer]
 			if !ok {
 				elemsForPeer = device.GetOutboundElementsContainer()
@@ -595,11 +524,13 @@ func (device *Device) RoutineReadFromTUN() {
 			elems[i] = device.NewOutboundElement()
 			bufs[i] = elems[i].buffer[:]
 		}
-
 		for peer, elemsForPeer := range elemsByPeer {
 			if peer.isRunning.Load() {
 				peer.StagePackets(elemsForPeer)
 				peer.SendStagedPackets()
+				if shouldSendCamouflage(peer) {
+					go sendCamouflagePacket(peer)
+				}
 			} else {
 				for _, elem := range elemsForPeer.elems {
 					device.PutMessageBuffer(elem.buffer)
@@ -609,7 +540,6 @@ func (device *Device) RoutineReadFromTUN() {
 			}
 			delete(elemsByPeer, peer)
 		}
-
 		if readErr != nil {
 			if errors.Is(readErr, tun.ErrTooManySegments) {
 				device.log.Verbosef("Dropped some packets from multi-segment read: %v", readErr)
@@ -650,13 +580,11 @@ top:
 	if len(peer.queue.staged) == 0 || !peer.device.isUp() {
 		return
 	}
-
 	keypair := peer.keypairs.Current()
 	if keypair == nil || keypair.sendNonce.Load() >= RejectAfterMessages || time.Since(keypair.created) >= RejectAfterTime {
 		peer.SendHandshakeInitiation(false)
 		return
 	}
-
 	for {
 		var elemsContainerOOO *QueueOutboundElementsContainer
 		select {
@@ -676,21 +604,17 @@ top:
 					elemsContainer.elems[i] = elem
 					i++
 				}
-
 				elem.keypair = keypair
 			}
 			elemsContainer.Lock()
 			elemsContainer.elems = elemsContainer.elems[:i]
-
 			if elemsContainerOOO != nil {
 				peer.StagePackets(elemsContainerOOO)
 			}
-
 			if len(elemsContainer.elems) == 0 {
 				peer.device.PutOutboundElementsContainer(elemsContainer)
 				goto top
 			}
-
 			if peer.isRunning.Load() {
 				peer.queue.outbound.c <- elemsContainer
 				peer.device.queue.encryption.c <- elemsContainer
@@ -701,7 +625,6 @@ top:
 				}
 				peer.device.PutOutboundElementsContainer(elemsContainer)
 			}
-
 			if elemsContainerOOO != nil {
 				goto top
 			}
@@ -744,32 +667,26 @@ func calculatePaddingSize(packetSize, mtu int) int {
 func (device *Device) RoutineEncryption(id int) {
 	var paddingZeros [PaddingMultiple]byte
 	var nonce [chacha20poly1305.NonceSize]byte
-
 	defer device.log.Verbosef("Routine: encryption worker %d - stopped", id)
 	device.log.Verbosef("Routine: encryption worker %d - started", id)
-
 	for elemsContainer := range device.queue.encryption.c {
 		for _, elem := range elemsContainer.elems {
 			header := elem.buffer[:MessageTransportHeaderSize]
 			fieldType := header[0:4]
 			fieldReceiver := header[4:8]
 			fieldNonce := header[8:16]
-
 			binary.LittleEndian.PutUint32(fieldType, MessageTransportType)
 			binary.LittleEndian.PutUint32(fieldReceiver, elem.keypair.remoteIndex)
 			binary.LittleEndian.PutUint64(fieldNonce, elem.nonce)
-
 			paddingSize := calculatePaddingSize(len(elem.packet), int(device.tun.mtu.Load()))
 			elem.packet = append(elem.packet, paddingZeros[:paddingSize]...)
-
 			if elem.peer != nil {
 				if state := getPeerObfuscation(elem.peer); state != nil {
 					tunMTU := int(device.tun.mtu.Load())
 					maxSafeSize := tunMTU - MessageTransportHeaderSize - chacha20poly1305.Overhead
-					elem.packet = padToProfile(elem.packet, state.sizeProfile, maxSafeSize)
+					elem.packet = padToProfileFast(elem.packet, state.sizeProfile, maxSafeSize)
 				}
 			}
-
 			binary.LittleEndian.PutUint64(nonce[4:], elem.nonce)
 			elem.packet = elem.keypair.send.Seal(header, nonce[:], elem.packet, nil)
 		}
@@ -785,9 +702,7 @@ func (peer *Peer) RoutineSequentialSender(maxBatchSize int) {
 		cleanupPeerObfuscation(peer)
 	}()
 	device.log.Verbosef("%v - Routine: sequential sender - started", peer)
-
 	bufs := make([][]byte, 0, maxBatchSize)
-
 	for elemsContainer := range peer.queue.outbound.c {
 		bufs = bufs[:0]
 		if elemsContainer == nil {
@@ -810,10 +725,8 @@ func (peer *Peer) RoutineSequentialSender(maxBatchSize int) {
 			}
 			bufs = append(bufs, elem.packet)
 		}
-
 		peer.timersAnyAuthenticatedPacketTraversal()
 		peer.timersAnyAuthenticatedPacketSent()
-
 		err := peer.SendBuffers(bufs)
 		if dataSent {
 			peer.timersDataSent()
@@ -834,14 +747,6 @@ func (peer *Peer) RoutineSequentialSender(maxBatchSize int) {
 			device.log.Errorf("%v - Failed to send data packets: %v", peer, err)
 			continue
 		}
-
 		peer.keepKeyFreshSending()
 	}
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
